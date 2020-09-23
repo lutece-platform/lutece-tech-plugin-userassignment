71ba1a4b228c01f3b0adea801e68cc6236e3bce2
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-09-23 16:43:46 +0200

    mvn license & format

--- src/java/fr/paris/lutece/plugins/userassignment/business/ResourceUser.java
+++ src/java/fr/paris/lutece/plugins/userassignment/business/ResourceUser.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2020, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.userassignment.business;
 
 import java.sql.Timestamp;
@@ -7,114 +40,121 @@ import fr.paris.lutece.portal.business.user.AdminUser;
 /**
  * This class represents an user assignment to a resource
  */
-public class ResourceUser {
+public class ResourceUser
+{
 
-	private int _id;
-	private int _idResource;
-	private String _resourceType;
-	private AdminUser _adminUser;
-	private Timestamp _dateAssignment;
-	private boolean _active;
-	
-	public ResourceUser( )
-	{
-		_adminUser = new AdminUser( );
-		_adminUser.setUserId( -1 );
-	}
+    private int _id;
+    private int _idResource;
+    private String _resourceType;
+    private AdminUser _adminUser;
+    private Timestamp _dateAssignment;
+    private boolean _active;
 
-	/**
-	 * @return the _id
-	 */
-	public int getId( )
-	{
-		return _id;
-	}
+    public ResourceUser( )
+    {
+        _adminUser = new AdminUser( );
+        _adminUser.setUserId( -1 );
+    }
 
-	/**
-	 * @param _id the _id to set
-	 */
-	public void setId( int _id )
-	{
-		this._id = _id;
-	}
+    /**
+     * @return the _id
+     */
+    public int getId( )
+    {
+        return _id;
+    }
 
-	/**
-	 * @return the _idResource
-	 */
-	public int getIdResource( )
-	{
-		return _idResource;
-	}
+    /**
+     * @param _id
+     *            the _id to set
+     */
+    public void setId( int _id )
+    {
+        this._id = _id;
+    }
 
-	/**
-	 * @param _idResource the _idResource to set
-	 */
-	public void setIdResource( int _idResource )
-	{
-		this._idResource = _idResource;
-	}
+    /**
+     * @return the _idResource
+     */
+    public int getIdResource( )
+    {
+        return _idResource;
+    }
 
-	/**
-	 * @return the _resourceType
-	 */
-	public String getResourceType( )
-	{
-		return _resourceType;
-	}
+    /**
+     * @param _idResource
+     *            the _idResource to set
+     */
+    public void setIdResource( int _idResource )
+    {
+        this._idResource = _idResource;
+    }
 
-	/**
-	 * @param _resourceType the _resourceType to set
-	 */
-	public void setResourceType( String _resourceType )
-	{
-		this._resourceType = _resourceType;
-	}
+    /**
+     * @return the _resourceType
+     */
+    public String getResourceType( )
+    {
+        return _resourceType;
+    }
 
-	/**
-	 * @return the _adminUser
-	 */
-	public AdminUser getAdminUser( )
-	{
-		return _adminUser;
-	}
+    /**
+     * @param _resourceType
+     *            the _resourceType to set
+     */
+    public void setResourceType( String _resourceType )
+    {
+        this._resourceType = _resourceType;
+    }
 
-	/**
-	 * @param _adminUser the _adminUser to set
-	 */
-	public void setAdminUser( AdminUser _adminUser )
-	{
-		this._adminUser = _adminUser;
-	}
+    /**
+     * @return the _adminUser
+     */
+    public AdminUser getAdminUser( )
+    {
+        return _adminUser;
+    }
 
-	/**
-	 * @return the _dateAssignmentDate
-	 */
-	public Timestamp getDateAssignment( )
-	{
-		return _dateAssignment;
-	}
+    /**
+     * @param _adminUser
+     *            the _adminUser to set
+     */
+    public void setAdminUser( AdminUser _adminUser )
+    {
+        this._adminUser = _adminUser;
+    }
 
-	/**
-	 * @param _dateAssignmentDate the _dateAssignmentDate to set
-	 */
-	public void setDateAssignment( Timestamp _dateAssignmentDate )
-	{
-		this._dateAssignment = _dateAssignmentDate;
-	}
+    /**
+     * @return the _dateAssignmentDate
+     */
+    public Timestamp getDateAssignment( )
+    {
+        return _dateAssignment;
+    }
 
-	/**
-	 * @return the _bIsActive
-	 */
-	public boolean isActive( )
-	{
-		return _active;
-	}
+    /**
+     * @param _dateAssignmentDate
+     *            the _dateAssignmentDate to set
+     */
+    public void setDateAssignment( Timestamp _dateAssignmentDate )
+    {
+        this._dateAssignment = _dateAssignmentDate;
+    }
 
-	/**
-	 * @param _bIsActive the _bIsActive to set
-	 */
-	public void setActive( boolean _bIsActive )
-	{
-		this._active = _bIsActive;
-	}
+    /**
+     * @return the _bIsActive
+     */
+    public boolean isActive( )
+    {
+        return _active;
+    }
+
+    /**
+     * @param _bIsActive
+     *            the _bIsActive to set
+     */
+    public void setActive( boolean _bIsActive )
+    {
+        this._active = _bIsActive;
+    }
 }
--- src/java/fr/paris/lutece/plugins/userassignment/business/ResourceUserHome.java
+++ src/java/fr/paris/lutece/plugins/userassignment/business/ResourceUserHome.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2020, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.userassignment.business;
 
 import java.util.List;
@@ -8,17 +41,18 @@ import fr.paris.lutece.portal.service.plugin.Plugin;
 import fr.paris.lutece.portal.service.plugin.PluginService;
 import fr.paris.lutece.portal.service.spring.SpringContextService;
 
-public final class ResourceUserHome {
+public final class ResourceUserHome
+{
 
-	private static Plugin _plugin = PluginService.getPlugin( UserassignmentPlugin.PLUGIN_NAME );
-	 
-	private static IResourceUserDAO _dao = SpringContextService.getBean( IResourceUserDAO.BEAN_NAME );
-	 
-	private ResourceUserHome( )
-	{
-	}
-	
-	/**
+    private static Plugin _plugin = PluginService.getPlugin( UserassignmentPlugin.PLUGIN_NAME );
+
+    private static IResourceUserDAO _dao = SpringContextService.getBean( IResourceUserDAO.BEAN_NAME );
+
+    private ResourceUserHome( )
+    {
+    }
+
+    /**
      * Creates a resource assignment
      * 
      * @param resourceUser
@@ -92,9 +126,9 @@ public final class ResourceUserHome {
      */
     public static void deactivateByUserResource( int userId, int resourceID, String resourceType )
     {
-        _dao.deactivateByUserResource( userId, resourceID, resourceType, _plugin);
+        _dao.deactivateByUserResource( userId, resourceID, resourceType, _plugin );
     }
-    
+
     /**
      * <p>
      * Delete the resource assignments associated to the specified resource.
@@ -110,5 +144,5 @@ public final class ResourceUserHome {
     {
         _dao.deleteAssignmentByResource( resourceID, resourceType, _plugin );
     }
-    
+
 }
