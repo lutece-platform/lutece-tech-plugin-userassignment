b3d99963e5bfc2a88f96743d013b8d53dd4fd1a8
Félix Dupriez <felix.dupriez@accenture.com>
2025-06-04 15:37:41 +0200

    LUT-30105: Add a method to return a distinct list of users filtered by the resource_type they're linked to

--- src/java/fr/paris/lutece/plugins/userassignment/business/ResourceUserDAO.java
+++ src/java/fr/paris/lutece/plugins/userassignment/business/ResourceUserDAO.java
@@ -55,6 +55,7 @@ public class ResourceUserDAO implements IResourceUserDAO
     private static final String SQL_DELETE = "DELETE FROM userassignment_resource_user WHERE id = ? ";
     private static final String SQL_SELECT_RESOURCE_BY_USER = "SELECT id, id_resource, resource_type, id_user, assignment_date, is_active FROM userassignment_resource_user WHERE id_user = ? AND resource_type = ? ";
     private static final String SQL_SELECT_USER_BY_RESOURCE = "SELECT id_user FROM userassignment_resource_user WHERE id_resource = ? AND resource_type = ? AND is_active = 1 ";
+    private static final String SQL_SELECT_USER_BY_RESOURCE_TYPE = "SELECT id_user FROM userassignment_resource_user WHERE resource_type = ? AND is_active = 1 ";
     private static final String SQL_DEACTIVATE_BY_USER_BY_RESOURCE = "UPDATE userassignment_resource_user set is_active = 0 WHERE id_user = ? AND id_resource = ? AND resource_type = ? ";
     private static final String SQL_DELETE_BY_RESOURCE = "DELETE FROM userassignment_resource_user WHERE id_resource = ? AND resource_type = ? ";
 
@@ -128,6 +129,25 @@ public class ResourceUserDAO implements IResourceUserDAO
         {
             int nIndex = 0;
             daoUtil.setInt( ++nIndex, resourceID );
+            daoUtil.setString( ++nIndex, resourceType );
+            daoUtil.executeQuery( );
+
+            while ( daoUtil.next( ) )
+            {
+                result.add( AdminUserHome.findByPrimaryKey( daoUtil.getInt( 1 ) ) );
+            }
+        }
+
+        return result;
+    }
+    
+    @Override
+    public List<AdminUser> selectUserListByResourceType( String resourceType, Plugin plugin )
+    {
+        List<AdminUser> result = new ArrayList<>( );
+        try ( DAOUtil daoUtil = new DAOUtil( SQL_SELECT_USER_BY_RESOURCE_TYPE, Statement.RETURN_GENERATED_KEYS, plugin ) )
+        {
+            int nIndex = 0;
             daoUtil.setString( ++nIndex, resourceType );
             daoUtil.executeQuery( );
 
